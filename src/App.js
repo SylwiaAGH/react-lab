155c43da740483ad2c6d28721d416dee443c4d7e
Student <student@d17-426a-pc-14.ipa.ii.agh.edu.pl>
2026-05-16 11:27:01 +0200

    reaktywność

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  let email= 'swaclawik@student.agh.edu.pl';
+    const [email, setEmail] = useState('fracz@agh.edu.pl');
+
+
     function handleChange(event) {
 
-        console.log(event.target.value);
+        setEmail(event.target.value);
     }
+    let message = '';
+    if (email.length<5) {
+        message = 'Ale masz krótki adres!'
+    } else if (email.length < 15) {
+        message = 'Twój adres e-mail jest w sam raz.'
+    } else {
+        message = 'Twój adres e-mail jest stanowczo za długi.'
+    }
+
     return (
     <div>
       <h1> System do zapisów na zajęcia </h1>
         <h2>Twój e-mail to {email}</h2>
+        {email.length >0 && <div> {message} </div>}
         <input type="text" onChange={handleChange}/>
+
 
     </div>
   );
