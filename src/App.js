75ccaef368a3dd089e8b50dee0e8c3edae829837
Student <student@d17-426a-pc-14.ipa.ii.agh.edu.pl>
2026-05-16 12:16:17 +0200

    Interakcje- proste logowanie

--- src/App.js
+++ src/App.js
@@ -4,31 +4,23 @@ import {useState} from "react";
 
 function App() {
     const [email, setEmail] = useState('fracz@agh.edu.pl');
-
-
-    function handleChange(event) {
-
-        setEmail(event.target.value);
-    }
-    let message = '';
-    if (email.length<5) {
-        message = 'Ale masz krótki adres!'
-    } else if (email.length < 15) {
-        message = 'Twój adres e-mail jest w sam raz.'
-    } else {
-        message = 'Twój adres e-mail jest stanowczo za długi.'
-    }
-
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     return (
     <div>
-      <h1> System do zapisów na zajęcia </h1>
-        <h2>Twój e-mail to {email}</h2>
-        {email.length >0 && <div> {message} </div>}
-        <input type="text" onChange={handleChange}/>
-        <button onClick= {()=> alert(email)}> Wyświetl mój adres </button>
+      <h1>Witaj w systemie do zapisów na zajęcia</h1>
+        { !isLoggedIn && <div>
+            <input type="text" onChange={(event) => setEmail (event.target.value)}/>
+            <button onClick= {()=> setIsLoggedIn (true)}> Zaloguj się </button>
 
+        </div>}
+        {isLoggedIn && <div>
+            <h1>Witaj {email }</h1>
+            <a onClick={() => setIsLoggedIn(false)}>Wyloguj</a>
+
+    </div>}
     </div>
+
   );
 }
 
